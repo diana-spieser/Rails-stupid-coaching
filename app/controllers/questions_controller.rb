642208b456cfcb25dc979c2b07e561e1418a8e92
diana spieser <dianaspieser@gmail.com>
2022-11-14 14:15:00 +0100

    ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @message = params[:ask]
+
+if @message == "I'm going to work"
+  @answer = "Great"
+elsif @message.include?('?')
+  @answer = "Silly question, get dressed and go to work!"
+else
+ @answer = "I don't care, get dressed and go to work!"
+ end
+end
+end
